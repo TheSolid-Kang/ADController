c8772c5c57ff506147f25567221c8da563998c5a
YONWOO\23040302 <caleb0109@yonwookorea.com>
2024-05-29 09:18:30 +0900

    * Update Submodule * ERP 업데이트 로직 안전성 추가: adYw_TADUsers_IFs[i].uSNCreated == erpYw_TADUsers_IFs[i].uSNCreated

--- ADController/01.CScene/CTestScene.cs
+++ ADController/01.CScene/CTestScene.cs
@@ -176,10 +176,8 @@ namespace ADController._01.CScene
             }
 
             //2. _TADUsers_IF 테이블에 데이터 INSERT, DELETE, UPDATE 쿼리 작성
-            //  가. INSERT
+            //  가. INSERT: AD에 있으나 ERP(yw_TADUsers_IF 테이블)에 없는 경우 -> ERP(yw_TADUsers_IF 테이블)에 AD 사용자 정보 등록
             {
-                //AD에 있으나 yw_TADUsers_IF 테이블에 없는 경우 
-                //-> yw_TADUsers_IF에 AD 사용자 정보 등록
                 List<yw_TADUsers_IF> insertYw_TADUsers_IFs = adYw_TADUsers_IFs.ToList();
                 erpYw_TADUsers_IFs.ForEach(AdNacUser =>
                 {
@@ -190,18 +188,15 @@ namespace ADController._01.CScene
                 using (var mgr = new MSSQL_Mgr())
                     insertYw_TADUsers_IFs.ForEach(insertYw_TADUsers_IF => mgr.InsertData<yw_TADUsers_IF>(ConfigurationManager.ConnectionStrings["YWDEV"].ConnectionString, insertYw_TADUsers_IF));
             }
-            //  나. DELETE
+            //  나. DELETE: ERP(yw_TADUsers_IF 테이블)에 있으나 AD에 없는 경우 -> ERP(yw_TADUsers_IF 테이블)에서 해당 데이터 삭제
             {
-                //yw_TADUsers_IF 테이블에 있으나 AD에 없는 경우
-                //-> yw_TADUsers_IF 테이블에서 해당 데이터 삭제
                 List<yw_TADUsers_IF> deleteYw_TADUsers_IFs = erpYw_TADUsers_IFs.ToList();
                 adYw_TADUsers_IFs.ForEach(yw_TADUsers_IF => deleteYw_TADUsers_IFs.RemoveAll(deleteYw_TADUsers_IF => deleteYw_TADUsers_IF.uSNCreated == yw_TADUsers_IF.uSNCreated));
                     
                 using (var mgr = new MSSQL_Mgr())
                     deleteYw_TADUsers_IFs.ForEach(deleteYw_TADUsers_IF => mgr.DeleteDataByKey<yw_TADUsers_IF>(ConfigurationManager.ConnectionStrings["YWDEV"].ConnectionString, deleteYw_TADUsers_IF.sAMAccountName));
             }
-            //  다. UPDATE
-            //  -> 최신 AD사용자 정보로 ERP의 yw_TADUsers_IF 업데이트
+            //  다. UPDATE: ERP(yw_TADUsers_IF 테이블)의 ROW가 AD사용자 정보와 다른 경우 -> 최신 AD사용자 정보로 ERP(yw_TADUsers_IF 테이블) 업데이트
             {
                 //1) 현재 AD 사용자 목록 컨테이너 재생성
                 adUsers = GetADUsers();
@@ -227,20 +222,12 @@ namespace ADController._01.CScene
                 
                 for(int i = 0; i < adYw_TADUsers_IFs.Count; ++i)
                 {
-                    if (false == aDUserComparer.Equals(adYw_TADUsers_IFs[i], erpYw_TADUsers_IFs[i]))
-                    {
-                        updateYw_TADUsers_IFs.Add(adYw_TADUsers_IFs[i]);
-                    }
+                    if(adYw_TADUsers_IFs[i].uSNCreated == erpYw_TADUsers_IFs[i].uSNCreated)
+                        if (false == aDUserComparer.Equals(adYw_TADUsers_IFs[i], erpYw_TADUsers_IFs[i]))
+                            updateYw_TADUsers_IFs.Add(adYw_TADUsers_IFs[i]);
                 }
                 using (var mgr = new MSSQL_Mgr())
-                {
-                    //updateYw_TADUsers_IFs.ForEach(updateYw_TADUsers_IF => mgr.UpdateDataByPrimaryKey<yw_TADUsers_IF>(ConfigurationManager.ConnectionStrings["YWDEV"].ConnectionString, updateYw_TADUsers_IF, new KeyValuePair<string, object>("uSNCreated", updateYw_TADUsers_IF.uSNCreated)));
                     updateYw_TADUsers_IFs.ForEach(updateYw_TADUsers_IF => mgr.UpdateData<yw_TADUsers_IF>(ConfigurationManager.ConnectionStrings["YWDEV"].ConnectionString, updateYw_TADUsers_IF, "uSNCreated"));
-                }
-
-
-
-
             }
 
 
